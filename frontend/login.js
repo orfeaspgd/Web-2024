b5f8e3a94bba838ee9bd1efd342c2edc53f25aa3
orfeaspg <orfpou527@gmail.com>
2024-08-14 10:50:47 +0300

    minor changes

--- frontend/login.js
+++ frontend/login.js
@@ -1,3 +1,25 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let formData = new URLSearchParams(new FormData(this)).toString();
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: formData
+    })
+        .then(response => response.json())
+        .then(data => {
+            let messageElement = document.getElementById('message');
+            if (data.status === 'success') {
+                window.location.href = data.redirectUrl;
+            } else {
+                messageElement.style.color = 'red';
+            }
+            messageElement.textContent = data.message;
+        })
+        .catch(error => console.error('Error:', error));
+});
 document.getElementById('createAccount').addEventListener('submit', function(event) {
     event.preventDefault();
     let formData = new URLSearchParams(new FormData(this)).toString();
