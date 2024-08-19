1f72e146fd6d6f3da14836a361768e6c12527563
Akis Papapanagiotou <akispapapanagiotou@hotmail.com>
2024-08-19 15:25:40 +0300

    Update login page and routes

--- routes/pages.js
+++ routes/pages.js
@@ -1,3 +1,9 @@
-export default function pagesRoutes(app) {
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+export default function pagesRoutes(app) {
+    app.get('/account-creation', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/html/citizen/create-account.html'));
+    });
 }
