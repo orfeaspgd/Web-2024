323768a190fcb016bf6c5197ea0a0ad3f24b3707
Akis Papapanagiotou <akispapapanagiotou@hotmail.com>
2024-08-28 00:22:11 +0300

    Add condition that checks if logout btn exists in shared.js file

--- frontend/js/shared.js
+++ frontend/js/shared.js
@@ -1,13 +1,17 @@
-//logout
-document.getElementById('logoutButton').addEventListener('click', function() {
-    fetch('/logout', {
-        method: 'POST'
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === 'success') {
-                window.location.href = data.redirectUrl;
-            }
+// Check if the logout button exists before adding the event listener
+const logoutButton = document.getElementById('logoutButton');
+
+if (logoutButton) {
+    logoutButton.addEventListener('click', function() {
+        fetch('/logout', {
+            method: 'POST'
         })
-        .catch(error => console.error('Error:', error));
-});
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    window.location.href = data.redirectUrl;
+                }
+            })
+            .catch(error => console.error('Error:', error));
+    });
+}
