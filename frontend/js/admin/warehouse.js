5588c3c84f1e533235840b3a00715e5d39c56975
Akis Papapanagiotou <akispapapanagiotou@hotmail.com>
2024-08-27 23:04:13 +0300

    Add active class for navbar + Center navbar + Add logout function in shared.js file

--- frontend/js/admin/warehouse.js
+++ frontend/js/admin/warehouse.js
@@ -81,20 +81,6 @@ pullProductsData = () => {
         .catch(error => console.error('Error:', error));
 }
 
-//logout
-document.getElementById('logoutButton').addEventListener('click', function() {
-    fetch('/logout', {
-        method: 'POST'
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === 'success') {
-                window.location.href = data.redirectUrl;
-            }
-        })
-        .catch(error => console.error('Error:', error));
-});
-
 //populate database with data from usidas
 document.getElementById('pullFromUsidas').addEventListener('click', function() {
     fetch('/pull_from_usidas', {
