7a8769022de76cb8d57a7a429ad4195c1cf86252
Akis Papapanagiotou <akispapapanagiotou@hotmail.com>
2024-08-30 04:13:03 +0300

    Create custom icons for markers

--- frontend/js/admin/map-view.js
+++ frontend/js/admin/map-view.js
@@ -1,5 +1,5 @@
 // Initialize the map
-let map = L.map('map').setView([38.246639, 21.734573], 15);
+const map = L.map('map').setView([38.246639, 21.734573], 14);
 
 // Add the OpenStreetMap tiles
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,3 +7,39 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Create custom icons for our markers
+const warehouseIcon = L.icon({
+    iconUrl: '../../assets/icons/warehouse.png',
+    iconSize: [25, 25]
+});
+
+const vehicleIcon = L.icon({
+    iconUrl: '../../assets/icons/vehicle.png',
+    iconSize: [25, 25]
+});
+
+const assignedRequestIcon = L.icon({
+    iconUrl: '../../assets/icons/assigned-request.png',
+    iconSize: [25, 25]
+});
+
+const pendingRequestIcon = L.icon({
+    iconUrl: '../../assets/icons/pending-request.png',
+    iconSize: [25, 25]
+});
+
+const assignedOfferIcon = L.icon({
+    iconUrl: '../../assets/icons/assigned-offer.png',
+    iconSize: [25, 25]
+});
+
+const pendingOfferIcon = L.icon({
+    iconUrl: '../../assets/icons/pending-offer.png',
+    iconSize: [25, 25]
+});
+
+
+
+// let warehouseMarker = L.marker([51.5, -0.09], { icon: warehouseIcon });
+//
+// warehouseMarker.addTo(map);
