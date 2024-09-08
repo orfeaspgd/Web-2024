bd1af4f5c6c63b35d83144f8365349dad94bd7d9
Akis Papapanagiotou <akispapapanagiotou@hotmail.com>
2024-09-08 13:22:58 +0300

    Add task-management file for routes

---
+++ routes/task-management.js
@@ -0,0 +1,29 @@
+import {
+    Users,
+    Announcements,
+    Products,
+    Tasks,
+    Categories,
+    WarehouseProducts,
+    Vehicles
+} from '../schemas.js';
+
+export default function taskManagementRoutes(app) {
+    // Helper function to calculate the distance between two points using the Haversine formula (in meters)
+    function calculateDistance(lat1, lon1, lat2, lon2) {
+        const R = 6371e3; // metres
+        const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
+        const φ2 = lat2 * Math.PI / 180;
+        const Δφ = (lat2 - lat1) * Math.PI / 180;
+        const Δλ = (lon2 - lon1) * Math.PI / 180;
+
+        const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return R * c; // in metres
+    }
+
+    
+}
