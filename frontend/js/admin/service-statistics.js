2f76ee26da51e54eb39a876fe66c7fd1303b4448
Akis Papapanagiotou <akispapapanagiotou@hotmail.com>
2024-09-11 08:46:15 +0300

    Improve UI

--- frontend/js/admin/service-statistics.js
+++ frontend/js/admin/service-statistics.js
@@ -51,13 +51,24 @@ async function displayTaskStatistics(startDate, endDate) {
                     data.completedRequestsCount,
                     data.completedOffersCount
                 ],
-                backgroundColor: ['rgba(54, 162, 235, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(153, 102, 255, 0.2)'],
-                borderColor: ['rgba(54, 162, 235, 1)', 'rgba(75, 192, 192, 1)', 'rgba(255, 159, 64, 1)', 'rgba(153, 102, 255, 1)'],
+                backgroundColor: [
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 128, 0, 0.5)',
+                    'rgba(0, 255, 255, 0.5)',
+                    'rgba(255, 255, 0, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(0, 0, 255, 1)',
+                    'rgba(0, 128, 0, 1)',
+                    'rgba(0, 255, 255, 1)',
+                    'rgba(255, 255, 0, 1)'
+                ],
                 borderWidth: 1
             }]
         },
         options: {
             responsive: true,
+            maintainAspectRatio: false,
             scales: {
                 y: {
                     beginAtZero: true
